443f0cb2eed2b49e50069dfb712cd61d27c2f869
AryaMerlet <151627770+AryaMerlet@users.noreply.github.com>
2026-03-03 14:41:51 +0100

    feat(crypto): add key generation and address derivation to keys.py

---
+++ src/crypto/keys.py
@@ -0,0 +1,24 @@
+import hashlib
+from cryptography.hazmat.primitives.asymmetric.ec import (
+    generate_private_key,
+    SECP256K1,
+    EllipticCurvePrivateKey
+)
+from cryptography.hazmat.primitives import serialization
+
+
+def generate_private_key() -> EllipticCurvePrivateKey:
+    return generate_private_key(SECP256K1())
+
+
+def derive_public_key(private_key: EllipticCurvePrivateKey) -> bytes:
+    return private_key.public_key().public_bytes(
+        encoding=serialization.Encoding.X962,
+        format=serialization.PublicFormat.UncompressedPoint
+    )
+
+
+def derive_address(public_key: bytes) -> str:
+    sha = hashlib.sha256(public_key).digest()
+    ripemd = hashlib.new("ripemd160", sha).digest()
+    return "ARC" + ripemd.hex()
