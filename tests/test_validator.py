bac677931f960bb8ea95e1f61755a843c7d7ceea
AryaMerlet <151627770+AryaMerlet@users.noreply.github.com>
2026-03-04 09:47:58 +0100

    feat(coverage): add missing validator edge case tests

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -141,3 +141,35 @@ def test_select_chain_picks_remote_if_longer():
         chain_b.add_block(block)
 
     assert Validator.select_chain(chain_a, chain_b) == chain_b
+
+def test_validate_transaction_wrong_signature():
+        private_key_a = keys.generate_private_key()
+        private_key_b = keys.generate_private_key()
+        address = keys.derive_address(keys.derive_public_key(private_key_a))
+        state = State()
+        state.balances[address] = 100.0
+        tx = Transaction(
+            sender=address,
+            recipient="ARCbob",
+            amount=10.0,
+            nonce=0,
+            sender_public_key=private_key_a.public_key()
+        )
+        # sign with wrong key
+        tx.signature = signatures.sign(private_key_b, tx.hash())
+        assert Validator.validate_transaction(tx, state) is False
+
+def test_validate_chain_invalid():
+            chain = Chain()
+            header1 = BlockHeader(index=0, prev_hash="0" * 64, merkle_root="0" * 64, difficulty=1)
+            block1 = Block(header1, [])
+            block1.mine()
+            chain.add_block(block1)
+
+            # second block with wrong prev_hash
+            header2 = BlockHeader(index=1, prev_hash="wronghash", merkle_root="0" * 64, difficulty=1)
+            block2 = Block(header2, [])
+            block2.mine()
+            chain.add_block(block2)
+
+            assert Validator.validate_chain(chain) is False
