b40b8c947e48513e3feeacdbe64422e08b96e743
AryaMerlet <151627770+AryaMerlet@users.noreply.github.com>
2026-03-04 09:21:21 +0100

    feat(tests): add transaction tests

---
+++ tests/test_transaction.py
@@ -0,0 +1,48 @@
+import pytest
+from src.crypto import keys, signatures
+from src.core.transaction import Transaction
+
+
+def test_transaction_hash_is_consistent():
+    private_key = keys.generate_private_key()
+    public_key = keys.derive_public_key(private_key)
+    address = keys.derive_address(public_key)
+    tx = Transaction(
+        sender=address,
+        recipient="ARCrecipient",
+        amount=10.0,
+        nonce=0,
+        sender_public_key=private_key.public_key()
+    )
+    assert tx.hash() == tx.hash()
+
+
+def test_transaction_signature():
+    private_key = keys.generate_private_key()
+    public_key = keys.derive_public_key(private_key)
+    address = keys.derive_address(public_key)
+    tx = Transaction(
+        sender=address,
+        recipient="ARCrecipient",
+        amount=10.0,
+        nonce=0,
+        sender_public_key=private_key.public_key()
+    )
+    tx.signature = signatures.sign(private_key, tx.hash())
+    assert signatures.verify(private_key.public_key(), tx.hash(), tx.signature) is True
+
+
+def test_transaction_to_dict():
+    private_key = keys.generate_private_key()
+    public_key = keys.derive_public_key(private_key)
+    address = keys.derive_address(public_key)
+    tx = Transaction(
+        sender=address,
+        recipient="ARCrecipient",
+        amount=10.0,
+        nonce=0,
+        sender_public_key=private_key.public_key()
+    )
+    d = tx.to_dict()
+    assert d["sender"] == address
+    assert d["amount"] == 10.0
